52954384d5bf79b96565851b52e887f3e154b112
Jorge Rodriguez <jarf4321@gmail.com>
2022-09-06 17:30:10 -0500

    Add - seed data for Admin, add faker gem

--- time_checker/db/seeds.rb
+++ time_checker/db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(
+  first_name: 'Omar',
+  last_name: 'Perez',
+  email: 'omar@gmail.com',
+  password: 'omar12345'
+)
+
+# # We have to create the 'Stores' first in order to apply the relation between tables
+# # Create Stores
+# 5.times do |i|
+#   puts "Creating store #{i}"
+#   Store.create(
+#     name: Faker::Company.name,
+#     address: Faker::Address.full_address
+#   )
+# end
+#
+#
+# # Crate Employees
+# 10.times do |i|
+#   puts "Creating employee #{i}"
+#   first_name_variable = Faker::Name.first_name
+#   store_id_variable = rand(1..5)
+#   Employee.create(
+#     first_name: first_name_variable,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.free_email(name: first_name_variable),
+#     position: Faker::Job.title,
+#     private_number: rand(1000..9999),
+#     store_id: store_id_variable,
+#     working: true
+#   )
+# end
+
+
