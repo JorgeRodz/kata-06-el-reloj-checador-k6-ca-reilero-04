04b6217b0940df23c4db6c7d4c361fed2a14596a
CalixtoLopez <calixto.lopez.santos@gmail.com>
2022-09-09 00:34:47 +0200

    - Create controller attendances Co-authored-by: dagp-ucol <dgentner@ucol.mx> Co-authored-by: JorgeRodz <jarf4321@gmail.com> Co-authored-by: GustavoHiguera <ghiguera@ucol.mx>

---
+++ time_checker/app/controllers/attendances_controller.rb
@@ -0,0 +1,39 @@
+class AttendanceController < ApplicationController
+    def show
+        @attendance =Attendance.find(params[:id])
+    end
+    
+      def index
+        @attendances = Attendance.all
+      end
+    
+    def new
+        @attendance = Attendance.new
+    end
+    
+      def create
+        @attendance = Attendance.new(params.require(:attendance).permit(:employee_id))
+        if @attendance.save
+          redirect_to @attendance
+        else
+          render 'new'
+        end
+      end
+    
+      def edit
+        @attendance = Attendance.find(params[:id])
+      end
+    #   def update
+    #     @store = Store.find(params[:id])
+    #     if @store.update(params.require(:store).permit(:name, :address))
+    #       redirect_to @store
+    #     else
+    #       render 'edit'
+    #     end
+    #   end
+    
+    #   def destroy
+    #     @store = Store.find(params[:id])
+    #     @store.destroy
+    #   end
+end
