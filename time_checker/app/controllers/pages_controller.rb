4f9694a9b8f02427c90013b1349bc7ffd6c94914
Jorge Rodriguez <jarf4321@gmail.com>
2022-09-15 16:06:03 -0500

    Add - check-in, check-out functionality:
    - Add 'attendance controller' methods for check-in, check-out
    - Add flash_messages
    
    Co-authored-by: GustavoHiguera <ghiguera@ucol.mx>
    Co-authored-by: dgentner <dgentner@ucol.mx>
    Co-authored-by: CalixtoLopez <calixto.lopez.santos@gmail.com>

--- time_checker/app/controllers/pages_controller.rb
+++ time_checker/app/controllers/pages_controller.rb
@@ -1,31 +1,4 @@
 class PagesController < ApplicationController
   def home
   end
-
-  def check_in(employee_id, private_number)
-    employee = Employee.find(employee_id)
-    if employee.private_number == private_number
-      if employee.working == false
-        redirect_to root_path, notice: "You are not working"
-      else
-        attendance = Attendance.create(employee_id: employee_id, check_in: Time.now, store_id: employee.store_id)
-        redirect_to root_path, notice: "Check in successful"
-      end
-    else
-      redirect_to root_path, notice: "Wrong private number"
-    end
-  end
-
-  def check_out
-    employee = Employee.find(employee_id)
-    if employee.private_number == private_number
-      if employee.working == false
-        redirect_to root_path, notice: "You are not working"
-      else
-        attendance = Attendance.find(employee_id: employee_id, check_out: nil)
-      end
-    else
-      redirect_to root_path, notice: "Wrong private number"
-    end
-  end
 end
